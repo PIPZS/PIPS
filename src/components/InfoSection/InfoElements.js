55d5fc79fe52733b778f4bc6dac0c943bc2920ff
sderewicki <sderewicki@vantek.co>
2022-03-27 11:16:27 -0300

    SebaDere | componente carousel

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -1,19 +1,44 @@
 import styled from "styled-components";
 
 
-export const InfoContainer = styled.div`
+
+export const MainWrapper = styled.div`
     background: black;
+
+    position:relative;
+`;
+
+export const Wrapper = styled.div`
+    top:0;
+    left:0;
+    height:100%;
+    width:100%;
+    display: flex;
+    flex-direction: column ;
+    position:absolute;
+`;
+
+
+export const InfoContainer = styled.div`
+    
     display: flex;
     justify-content: center;
     padding: 50px 24px;
     position: relative;
-    padding-top 5%;
+    padding-top: 5%;
 
     @media screen and (max-width:1200px){
         display: flex;
         flex-direction: column;
     }
 `
+
+
+export const CarouselContainer = styled.div`
+
+
+`;
+
 
 export const InfoWrapper = styled.div`
     justify-content: left;
@@ -73,7 +98,6 @@ export const InfoCardsWrapper = styled.div`
     @media screen and (max-width: 1200px){
         width: 80%;
         margin: auto;
-        margin-bottom: 5%;
         margin-top: 5%;
     }   
 
