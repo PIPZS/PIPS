026c95f0f8af0645b62eaff0944eab4640fc39b0
sderewicki <sderewicki@vantek.co>
2022-04-10 15:41:41 -0300

    SebaDere | cambios de flipcard

--- src/components/InfoSection/FlipCard.js
+++ src/components/InfoSection/FlipCard.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./flipCard.css"
 
 function FlipCard({ card, icon}) {
+   // Declare a new state variable with the "useState" Hook
+   const [width, setWidth] = React.useState(window.innerWidth);
+   const breakpoint = 620;
+ 
+   React.useEffect(() => {
+     /* Inside of a "useEffect" hook add an event listener that updates
+        the "width" state variable when the window size changes */
+     window.addEventListener("resize", () => setWidth(window.innerWidth));
+ 
+     /* passing an empty array as the dependencies of the effect will cause this
+        effect to only run when the component mounts, and not each time it updates.
+        We only want the listener to be added once */
+   }, []);
+
 
   
   return (
