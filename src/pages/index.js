2051e7970d7e26b60fbdc184dc3e03b1db762703
sebadere <dere1997@gmail.com>
2022-05-18 18:07:52 -0300

    Ultimops cambios

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,6 @@ const Home = () => {
             <HomeSection/>
             <InfoSection theme={theme}/>
             <ServicesSection/>
-            
             <ContactSection/>
             <Footer/>
         </ThemeProvider>
