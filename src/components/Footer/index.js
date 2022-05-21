d7890831b09a3decdf4fac930749cb0c8b4bb004
sebadere <dere1997@gmail.com>
2022-05-20 21:34:04 -0300

    Cambios

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,7 +18,7 @@ const Footer = () => {
                     <SocialMediaWrapper>
                     <LeftContainer>
                             <Title alt to='/' onClick={toggleHome}>PIPS LAB</Title>
-                            <Subtitle>Desafiando el statu quo</Subtitle>
+                            <Subtitle>Desafiando el status quo</Subtitle>
                         </LeftContainer>
                         <RightContainer>
                             <SocialLinks>
